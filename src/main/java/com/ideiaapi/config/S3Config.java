fc8b1e29b369430a3ed12c792e9f1cef2a8f9c59
Alex Alves de Souza <alex.souza@zup.com.br>
2019-01-06 17:25:15 -0200

    add bucket e expirationdys

--- src/main/java/com/ideiaapi/config/S3Config.java
+++ src/main/java/com/ideiaapi/config/S3Config.java
@@ -8,6 +8,11 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.BucketLifecycleConfiguration;
+import com.amazonaws.services.s3.model.CreateBucketRequest;
+import com.amazonaws.services.s3.model.Tag;
+import com.amazonaws.services.s3.model.lifecycle.LifecycleFilter;
+import com.amazonaws.services.s3.model.lifecycle.LifecycleTagPredicate;
 
 @Configuration
 public class S3Config {
@@ -48,10 +53,31 @@ public class S3Config {
 
         AWSCredentials credentials = new BasicAWSCredentials(s3AccessKey, s3SecretAccessKey);
 
-        return AmazonS3ClientBuilder.standard()
+        AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .build();
 
+        this.criaBucketAutomaticmente(amazonS3);
+
+        return amazonS3;
+
+    }
+
+    private void criaBucketAutomaticmente(AmazonS3 amazonS3) {
+        if (!amazonS3.doesBucketExistV2(bucket)) {
+            amazonS3.createBucket(new CreateBucketRequest(bucket));
+
+            BucketLifecycleConfiguration.Rule regraDeExpiracao = new BucketLifecycleConfiguration.Rule()
+                    .withId("Regra de expiração para arquivos temporários")
+                    .withFilter(new LifecycleFilter(new LifecycleTagPredicate(new Tag("expirar", "true"))))
+                    .withExpirationInDays(1)
+                    .withStatus(BucketLifecycleConfiguration.ENABLED);
+
+            BucketLifecycleConfiguration configuration = new BucketLifecycleConfiguration()
+                    .withRules(regraDeExpiracao);
+
+            amazonS3.setBucketLifecycleConfiguration(bucket, configuration);
+        }
     }
 
 }
