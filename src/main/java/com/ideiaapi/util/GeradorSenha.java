38677e5d93ab7209dedf024ccc8e495ae89b44bb
Alex Alves de Souza <alex.souza@zup.com.br>
2019-03-13 21:41:23 -0300

    ajuste

--- src/main/java/com/ideiaapi/util/GeradorSenha.java
+++ src/main/java/com/ideiaapi/util/GeradorSenha.java
@@ -6,7 +6,7 @@ public class GeradorSenha {
 
 	public static void main(String[] args) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		System.out.println(encoder.encode("caj29al05"));
+		System.out.println(encoder.encode("secretaria@123"));
 	}
 
 }
